9a43b6573aa2b727d8cd77e192ce0f3380862580
Sergio Terrino <sergioterrino8@gmail.com>
2023-05-30 11:05:26 +0200

    3 commit - Creo las entradas

--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/entrada';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+
+  public listadoEntradas: Entrada[];
+
+  constructor(){
+    this.listadoEntradas = [
+      {
+        titulo: 'Introducción a Angular',
+        resumen: 'En esta lección realizaremos una pequeña introducción a este framework'
+      },
+      {
+        titulo: 'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje de Typescript como base'
+      },
+      {
+        titulo: 'Componenentes en Angular',
+        resumen: 'Aprenderemos a usar los componentes en Angular y el p'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
