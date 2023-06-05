f4ef3a21f421c468c89520d574afc66fae40cee1
Sergio Terrino <sergioterrino8@gmail.com>
2023-06-05 19:15:06 +0200

    1 commit - creo la navegacion mediante los routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ListadoComponent } from './views/listado/listado.component';
+import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
 
 const routes: Routes = [
   {path: 'listado', component: ListadoComponent},
+  {path: 'nosotros', component: AcercaDeNosotrosComponent},
 
   {path: '', redirectTo: '/listado', pathMatch: 'full'}, //ruta por defecto para la primera vez que accedemos a la web
-  //{path: '**', component: PaginaNoEncontradaComponent} //tendriamos que crear dicho componente
+  {path: '**', component: PaginaNoEncontradaComponent}
 ];
 
 @NgModule({
